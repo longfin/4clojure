19861c58f61e36e4aac161e80371b66729b1511e
longfin <longfinfunnel@gmail.com>
2012-09-06 22:13:39 +0900

    modify load-props to using clojure.java.io

--- src/foreclojure/messages.clj
+++ src/foreclojure/messages.clj
@@ -1,10 +1,11 @@
-(ns foreclojure.messages)
+(ns foreclojure.messages
+  (:use [clojure.java.io :only [resource input-stream]]))
 
-(defn load-props [file] 
-  (into {} (doto (java.util.Properties.)
-             (.load (-> (Thread/currentThread)
-             (.getContextClassLoader)
-             (.getResourceAsStream file))))))
+(defn load-props [file-name]
+  (let [^java.net.URL url (resource file-name)]
+    (with-open [^java.io.BufferedInputStream stream (input-stream url)]
+      (into {} (doto (java.util.Properties.)
+                 (.load stream))))))
  
 (def err-msg-map  (load-props "error-messages.properties"))             
              
